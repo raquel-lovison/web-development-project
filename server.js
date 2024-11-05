b09578c399d34cb51ff67cb4c7b188b198f78ea2
Raquel Lovison <raquelovison@gmail.com>
2024-11-05 18:40:40 -0500

    change the contact form to use json

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express'); // Added the Express library to create the server and handle requests
+const bodyParser = require('body-parser'); // Library that parses the body of the request
 const app = express(); // Creates an Express app that will determine how our server works
 
 const PORT = 3000; // Set the server's incoming request port & the server will run on http://localhost:3000
 
-app.use(express.static('public')) //Serve static files from the public directory
-app.use(express.urlencoded({ extended: true})); // Form info is made available in req.body so it can be processed
+app.use(express.static('public')) // Serve static files from the public directory
+app.use(bodyParser.json()); // Enable json parsing in the body of the request
 
 // Handles client-side contact form submissions, and also handle 'post' requests that come from "/send"
 // Get the info from the request body: name, email and message 
 app.post('/send', (req, res) => {
     const { name, email, message } = req.body;
+
+    // Check if all fields 
+    if (!name || !email || !message) {
+        return res.status(400).json({ message: "All fields are required." });
+    }
 
 // Displays the info from the contact form in the terminal
     console.log(`Name: ${name}`);
@@ -18,7 +24,7 @@ app.post('/send', (req, res) => {
     console.log('----');
 
 // Shows a success message back to the user's browser 
-    res.send('Success!! Thank you for your message.');
+    res.status(200).json({message:'Success!! Thank you for your message.'});
 });
 
 // Starts the server
